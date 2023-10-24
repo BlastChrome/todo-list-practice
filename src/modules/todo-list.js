78a0ea983eb1a5b6b205391c85949185b5263437
Nicholas Jackson <nick@Nicholass-MacBook-Pro.local>
2023-10-24 12:51:13 -0700

    Add todo list functionality

---
+++ src/modules/todo-list.js
@@ -0,0 +1,36 @@
+import pubsub from '../../js/pubsub.js'; // Import your PubSub class or module
+
+export default class TodoList {
+    constructor() {
+        this.list = [];
+        this.subscribe();
+    }
+
+    // subscriptions 
+    subscribe = () => {
+        pubsub.subscribe("addTodo", this.addTodo);
+        pubsub.subscribe('removeTodo', this.removeTodo);
+        pubsub.subscribe('updateTodo', this.updateTodo);
+    }
+
+    addTodo = (newTodo) => {
+        this.list.push(newTodo);
+        pubsub.publish("todoAdded", newTodo);
+    }
+
+    removeTodo = (todoId) => {
+        const index = this.list.findIndex(todo => todo.id === todoId);
+        if (index > -1) {
+            const removedTodo = this.list.splice(index, 1);
+            pubsub.publish('todoRemoved', removedTodo[0]);
+        }
+    }
+
+    updateTodo = (todoId, newText) => {
+        const todoToUpdate = this.list.find(todo => todo.id === todoId);
+        if (todoToUpdate) {
+            todoToUpdate.text = newText;
+            pubsub.publish("todoUpdated", todoToUpdate);
+        }
+    }
+}
