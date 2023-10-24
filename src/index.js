ee61777b942e82dcefceebf969ce9be114837701
Nicholas Jackson <nick@Nicholass-MacBook-Pro.local>
2023-10-24 09:30:24 -0700

    added tests for the todo class

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import { pubsub } from "../js/pubsub.js";
+import Todo from "../src/modules/todo.js"
 
 const publish = pubsub.publish.bind(pubsub);
 const subscribe = pubsub.subscribe.bind(pubsub);
 
+
 const App = (() => {
 
 })();
-2
