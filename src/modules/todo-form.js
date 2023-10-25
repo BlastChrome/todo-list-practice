001d8e9ca71cba50551b41c8d961cfa3dc90b386
Nicholas Jackson <nick@Nicholass-MacBook-Pro.local>
2023-10-25 08:31:09 -0700

    added basic functionality for todo-form

---
+++ src/modules/todo-form.js
@@ -0,0 +1,17 @@
+import { pubsub } from '../../js/pubsub.js'; // Import your PubSub class or module
+
+export default class TodoForm {
+    constructor() {
+        this.todoInput = "";
+        this.subscribe();
+    }
+
+    subscribe = () => {
+        pubsub.subscribe("validateInput", this.handleTodoInput)
+    }
+    handleTodoInput = (text) => {
+        if (text.length <= 0) return;
+        this.todoInput = text;
+        pubsub.publish("addTodo", this.todoInput);
+    }
+}
